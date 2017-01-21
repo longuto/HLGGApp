28cebd90c57ecf16478accc867df0b20bdee1222
longuto <1592404240@qq.com>
2017-01-21 17:36:43 +0800

    2017.1.21

---
+++ app/src/main/java/com/simpotech/app/hlgg/ui/widget/MyRecyclerView.java
@@ -0,0 +1,26 @@
+package com.simpotech.app.hlgg.ui.widget;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+/**
+ * Created by longuto on 2017/1/21.
+ */
+
+public class MyRecyclerView extends RecyclerView {
+
+    public MyRecyclerView(Context context) {
+        super(context);
+    }
+
+    public MyRecyclerView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyRecyclerView(Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+}
